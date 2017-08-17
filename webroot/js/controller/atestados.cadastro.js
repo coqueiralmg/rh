5a0e386b36c7aa7f5999bd2bc744ba6f98edd572
Fábio Valentim <maitabom@gmail.com>
2017-08-17 14:44:17 -0300

    Criação do autocomplete e validações do formulário de popup

--- webroot/js/controller/atestados.cadastro.js
+++ webroot/js/controller/atestados.cadastro.js
@@ -14,4 +14,55 @@ $(function () {
     $('#data_emissao').mask('00/00/0000');
     $('#data_afastamento').mask('00/00/0000');
     $('#data_retorno').mask('00/00/0000');
+    $('#crm').mask('00000000000');
+
+    $('#nome_funcionario').autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: '/rh/funcionarios/listar',
+                dataType: 'json',
+                data: {
+                    nome: request.term
+                },
+                success: function (data) {
+                    response(data);
+                }
+            });
+        },
+        select: function (event, ui) {
+            $('#nome_funcionario').val(ui.item.nome);
+            $('#id_funcionario').val(ui.item.id);
+
+            return false;
+        }
+    }).autocomplete("instance")._renderItem = function (ul, item) {
+        return $("<li>")
+            .append(item.nome)
+            .appendTo(ul);
+    };
 });
+
+function salvarMedico(){
+    if(!validarPopup()) return false;
+
+    
+}
+
+function validarPopup() {
+    var mensagem = "";
+
+    if ($("#nome").val() === "") {
+        mensagem += "<li> É obrigatório informar o nome do médico.</li>";
+        $("label[for='nome']").css("color", "red");
+    } else {
+        $("label[for='nome']").css("color", "#aaa");
+    }
+
+    if (mensagem == "") {
+        return true;
+    } else {
+        $("#cadastro_erro_popup").show('shake');
+        $("#cadastro_erro_popup #details").html("<ol>" + mensagem + "</ol>");
+        return false;
+    }
+}
