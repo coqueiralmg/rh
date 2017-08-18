63f3a0241f5c62352777c480f632dccd44ddbe4d
Fábio Valentim <maitabom@gmail.com>
2017-08-18 09:44:34 -0300

    Implementação de validação de cadastro de atestados médicos

--- webroot/js/controller/atestados.cadastro.js
+++ webroot/js/controller/atestados.cadastro.js
@@ -62,13 +62,13 @@ $(function () {
         }
     }).autocomplete("instance")._renderItem = function (ul, item) {
         return $("<li>")
-            .append('<span><b>' + item.nome + '</b><span><br/><small>'+ item.especialidade + ' CRM:' + item.crm + '</small>')
+            .append('<span><b>' + item.nome + '</b><span><br/><small>' + item.especialidade + ' CRM:' + item.crm + '</small>')
             .appendTo(ul);
     };;
 });
 
-function salvarMedico(){
-    if(!validarPopup()) return false;
+function salvarMedico() {
+    if (!validarPopup()) return false;
 
     var nome = $("#nome").val();
     var crm = $("#crm").val();
@@ -82,7 +82,7 @@ function salvarMedico(){
             crm: crm,
             especialidade: especialidade
         },
-        success: function(data) {
+        success: function (data) {
             $("#cadastro_sucesso_popup").show('fade');
             $("#btnSalvaMedico").hide();
         }
@@ -91,8 +91,8 @@ function salvarMedico(){
 
 function fecharModalMedico() {
     $("#btnSalvaMedico").show();
-    $("#nome").val('');   
-    $("#crm").val(''); 
+    $("#nome").val('');
+    $("#crm").val('');
     $("#especialidade").val('');
     $("#cadastro_sucesso_popup").hide();
     $("#cadastro_erro_popup").hide();
@@ -113,6 +113,88 @@ function validarPopup() {
     } else {
         $("#cadastro_erro_popup").show('shake');
         $("#cadastro_erro_popup #details").html("<ol>" + mensagem + "</ol>");
+        return false;
+    }
+}
+
+function validar() {
+    var mensagem = "";
+
+    if ($("#id_funcionario").val() === "") {
+        mensagem += "<li> É obrigatório informar o nome do funcionário cadastrado no sistema.</li>";
+        $("label[for='funcionario']").css("color", "red");
+    } else {
+        $("label[for='funcionario']").css("color", "#aaa");
+    }
+
+    if ($("#data_emissao").val() === "") {
+        mensagem += "<li> É obrigatório informar a data de emissão do atestado.</li>";
+        $("label[for='data-emissao']").css("color", "red");
+    } else {
+        $("label[for='data-emissao']").css("color", "#aaa");
+    }
+
+    if ($("#data_afastamento").val() === "") {
+        mensagem += "<li> É obrigatório informar a data de afastamento do funcionário.</li>";
+        $("label[for='data-afastamento']").css("color", "red");
+    } else {
+        $("label[for='data-afastamento']").css("color", "#aaa");
+    }
+
+    if ($("#data_retorno").val() === "") {
+        mensagem += "<li> É obrigatório informar a data de retorno do funcionário.</li>";
+        $("label[for='data-retorno']").css("color", "red");
+    } else {
+        $("label[for='data-retorno']").css("color", "#aaa");
+    }
+
+    if($("#data_afastamento").val() !== "" && $("#data_retorno").val() !== "") {
+        var afastamento = new Date($("#data_afastamento").val());
+        var retorno = new Date($("#data_retorno").val());
+
+        if(afastamento > retorno) {
+            mensagem += "<li> A data de afastamento é maior do que a data de retorno.</li>";
+            $("label[for='data-afastamento']").css("color", "red");
+            $("label[for='data-retorno']").css("color", "red");
+        } else {
+            $("label[for='data-afastamento']").css("color", "#aaa");
+            $("label[for='data-retorno']").css("color", "#aaa");
+        }
+    }
+
+    if ($("#quantidade_dias").val() === "") {
+        mensagem += "<li> É obrigatório informar a quantidade de dias de afastamento, determinado pelo atestado.</li>";
+        $("label[for='quantidade-dias']").css("color", "red");
+    } else {
+        $("label[for='quantidade-dias']").css("color", "#aaa");
+    }
+
+    if ($("#id_medico").val() === "") {
+        mensagem += "<li> É obrigatório informar o nome do médico cadastrado no sistema. Você pode adicionar o novo médico, clicando em 'Adicionar médico'</li>";
+        $("label[for='medico']").css("color", "red");
+    } else {
+        $("label[for='medico']").css("color", "#aaa");
+    }
+
+    if ($("#cid").val() === "") {
+        mensagem += "<li> É obrigatório informar o CID do atestado.</li>";
+        $("label[for='cid']").css("color", "red");
+    } else {
+        $("label[for='cid']").css("color", "#aaa");
+    }
+
+    if ($("#motivo").val() === "") {
+        mensagem += "<li> É obrigatório informar o motivo do afastamento.</li>";
+        $("label[for='motivo']").css("color", "red");
+    } else {
+        $("label[for='motivo']").css("color", "#aaa");
+    }
+
+    if (mensagem == "") {
+        return true;
+    } else {
+        $("#cadastro_erro").show('shake');
+        $("#cadastro_erro #details").html("<ol>" + mensagem + "</ol>");
         return false;
     }
 }
