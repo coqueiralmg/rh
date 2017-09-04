2753ba37e7b0611e47d3d04022134723aa075120
Fábio Valentim <maitabom@gmail.com>
2017-09-04 08:15:04 -0300

    Implementação de limpeza de cache, ao acionar o System Reset

--- src/Controller/SystemController.php
+++ src/Controller/SystemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Cake\Cache\Cache;
 use Cake\Core\Configure;
 use Cake\Network\Session;
 use Cake\ORM\TableRegistry;
@@ -224,6 +225,7 @@ class SystemController extends AppController
         $this->Auditoria->registrar($auditoria);
         
         $this->request->session()->destroy();
+        Cache::clear(false);
         $this->redirectLogin("As sessões foram zeradas com sucesso.", false);
     }
 
