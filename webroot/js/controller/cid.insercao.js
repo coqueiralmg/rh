52b56364756228f9fbe69758b403015e889cdae4
Fábio Valentim <maitabom@gmail.com>
2018-02-21 15:55:22 -0300

    Desenvolvimento da validação de dados de insersão de CID em massa

--- webroot/js/controller/cid.insercao.js
+++ webroot/js/controller/cid.insercao.js
@@ -58,3 +58,85 @@ function removerLinha(o) {
         }
     }
 }
+
+function validar() {
+    var tabela = document.getElementById("tblCadastro");
+    var mensagem = "";
+    var i = 1;
+
+    while(i < tabela.rows.length){
+        var linha = tabela.rows[i];
+        var campos = tabela.getElementsByTagName("input");
+        
+        var codigo = obterCampo(campos, "codigo");
+        var nome = obterCampo(campos, "nome");
+
+        if(codigo.value == "") {
+            mensagem += "<li>[Linha " + i + "] É obrigatório informar o código do CID.</li>";
+            codigo.style.color = "red";
+        } else {
+            codigo.style.color = "#555";
+        }
+
+        if(nome.value == "") {
+            mensagem += "<li>[Linha " + i + "] É obrigatório informar o nome da doença ou problema relacionado ao CID.</li>";
+            nome.style.color = "red";
+        } else {
+            nome.style.color = "#555";
+        }   
+
+        i++;
+    }
+
+
+    if (mensagem == "") {
+        return true;
+    } else {
+        montarLinhasTabela(tabela);
+        $("#cadastro_erro").show('shake');
+        $("#details").html("<ol>" + mensagem + "</ol>");
+        return false;
+    }
+}
+
+function montarLinhasTabela(tabela){
+    var i = 0;
+
+    do{
+        var linha = tabela.rows[i];
+        var celula = null;
+
+        if(linha.cells.length == 5) {
+            if(i > 0) {
+                celula = linha.cells[0];
+                celula.innerHTML = i;
+            }
+        } else {
+            if(i == 0) {
+                var pivot = linha.cells[0];
+                celula = document.createElement("th");
+                celula.innerHTML = "Linha"
+                celula.style.width = "7%";
+    
+                linha.insertBefore(celula, pivot);
+            } else {
+                celula = linha.insertCell(0);
+                celula.innerHTML = i;
+            }    
+        }
+        
+        i++;
+    }while(i < tabela.rows.length);
+}
+
+function obterCampo(campos, id){
+    var i = 0;
+
+    do {
+        if(campos[i].id = id) {
+            return campos[i];
+        }
+
+        i++;
+    } while(i < campos.length);
+}
